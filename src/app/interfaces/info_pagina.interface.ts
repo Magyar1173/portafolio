30d6dfcf073a964036b7c0c9ee9cb6ad95fce4be
Alberto Szendrey <Alberto.Szendrey@arcelormittal.com.ar>
2020-05-20 09:59:48 -0300

    Fin de sección 4

---
+++ src/app/interfaces/info_pagina.interface.ts
@@ -0,0 +1,11 @@
+export interface InfoPagina {
+titulo?: string;
+mail?: string;
+nombre_corto?: string;
+pagina_autor?: string;
+facebook?: string;
+twitter?: string;
+instagram?: string;
+tumbrl?: string;
+equipo_trabajo?: any[];
+}
