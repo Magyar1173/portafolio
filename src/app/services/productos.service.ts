3794a475d8bc8d27b4e07439befc95b6ee1a9445
Alberto Szendrey <Alberto.Szendrey@arcelormittal.com.ar>
2020-07-27 20:03:27 -0300

    Listo para producción

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,19 +8,55 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
 cargando = true;
 productos: Producto[] = [];
+productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient) {
     this.cargarProductos();
    }
 
-private cargarProductos(){
-  this.http.get('https://angular-html-fc095.firebaseio.com/productos_idx.json')
-  .subscribe((resp: Producto[] ) => {
-
-    console.log(resp);
+private cargarProductos() {
+return new Promise((resolve, reject) => {
+    this.http.get('https://angular-html-fc095.firebaseio.com/productos_idx.json')
+  .subscribe( (resp: Producto[] ) => {
     this.productos = resp;
     this.cargando = false;
-
+    resolve();
   });
+});
+
+}
+getProducto( id: string) {
+ return this.http.get(`https://angular-html-fc095.firebaseio.com/productos/${ id }.json`)
+}
+
+buscarProducto( termino: string) {
+  if ( this.productos.length === 0){
+    //cargar productos
+    this.cargarProductos().then(() => {
+      //ejecutar después de tener los productos
+      //aplicar filtro
+      this.filtrarProductos( termino );
+    });
+
+  } else {
+    //aplicar filtro
+    this.filtrarProductos( termino );
+  }
+
+
+}
+
+private filtrarProductos( termino: string) {
+console.log(this.productos);
+this.productosFiltrado = [];
+
+termino = termino.toLocaleLowerCase();
+
+this.productos.forEach( prod => {
+  const tituloLower = prod.titulo.toLocaleLowerCase();
+  if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+    this.productosFiltrado.push( prod );
+  }
+});
 }
 }
